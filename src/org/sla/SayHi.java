51dee64c4e421fd246b92babc89e055a2c462b7c
vhernandez <vhernandez@scienceleadership.org>
2019-09-20 11:48:54 -0400

    Add narration to the conversation.

--- src/org/sla/SayHi.java
+++ src/org/sla/SayHi.java
@@ -4,5 +4,6 @@ public class SayHi {
 
     public static void main(String[] args) {
         System.out.println("Mr. Hernandez says, \"Hi!\"");
+        System.out.println("Is he alone?  Does everyone have headphones on?");
     }
 }
