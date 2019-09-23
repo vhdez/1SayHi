0a22ccb02a8feee0ee6e84480122f13c4372700d
jackmotter <jackmotter3@gmail.com>
2019-09-23 14:35:44 -0400

    I replied

--- src/org/sla/SayHi.java
+++ src/org/sla/SayHi.java
@@ -6,5 +6,6 @@ public class SayHi {
         System.out.println("Mr. Hernandez says, \"Hi!\"");
         System.out.println("Is he alone?  Does everyone have headphones on?");
         System.out.println("Hi Mr. Hernandez its me Sal I am also here now. I don't have headphones on.");
+        System.out.println("Jack: \"I'm as well Mr. Hernandez\"");
     }
 }
