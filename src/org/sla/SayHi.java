fe17df9ca29fc1741486377aae628466527bdb9a
Salvc <scamarote20@scienceleadership.org>
2019-09-23 14:43:36 -0400

    I said hello!

--- src/org/sla/SayHi.java
+++ src/org/sla/SayHi.java
@@ -8,5 +8,7 @@ public class SayHi {
         System.out.println("Hi Mr. Hernandez its me Sal I am also here now. I don't have headphones on.");
         System.out.println("Hey!! It's Ethan. I guess I'm a little late. I took my headphones out before class started.");
         System.out.println("Jack: \"I'm here as well Mr. Hernandez\"");
+        System.out.println("Jack: \"I'm really bad at smash\"");
+
     }
 }
